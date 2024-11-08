2b70e31ea5dbb683afc259d6de0314186c038ee6
monichkirill <kirillmonich13@icloud.com>
2024-11-08 17:14:16 +0300

    hm11

--- src/ApplicationRunner.java
+++ src/ApplicationRunner.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import operation.StringOperation;
 
 public class ApplicationRunner {
+    public static void main(String[] args) {
+        String docNum = "1234-abc-5678-def-9x0y";
+        System.out.println(StringOperation.first(docNum));
+        System.out.println(StringOperation.second(docNum));
+        System.out.println(StringOperation.third(docNum));
+        System.out.println(StringOperation.fourth(docNum));
+        System.out.println(StringOperation.fifth(docNum));
+        System.out.println(StringOperation.sixth(docNum));
+        System.out.println(StringOperation.seventh(docNum));
+    }
 }
--- src/operation/StringOperation.java
+++ src/operation/StringOperation.java
@@ -1,4 +1,39 @@
 package operation;
 
 public class StringOperation {
+    public static String first(String docNumber) {
+        String[] parts = docNumber.split("-");
+        return parts[0] + " " + parts[2];
+    }
+
+    public static String second(String docNumber) {
+        return docNumber.replaceAll("[a-zA-Z]{3}", "***");
+    }
+
+    public static String third(String docNumber) {
+        String[] parts = docNumber.split("-");
+        return parts[0].toLowerCase() + "/" + parts[1].toLowerCase() + "/" + parts[2].toLowerCase() + "/" + parts[3].toLowerCase();
+    }
+
+    public static String fourth(String docNumber) {
+        String[] parts = docNumber.split("-");
+        StringBuilder sb = new StringBuilder("Letters:");
+        sb.append(parts[0].toUpperCase()).append("/")
+                .append(parts[1].toUpperCase()).append("/")
+                .append(parts[2].substring(0,1).toUpperCase()).append("/")
+                .append(parts[3].substring(2,3).toUpperCase()).append("/");
+        return sb.toString();
+    }
+
+    public static boolean fifth(String docNumber) {
+        return docNumber.toLowerCase().contains("abc");
+    }
+
+    public static boolean sixth(String docNumber) {
+        return docNumber.startsWith("555");
+    }
+
+    public static boolean seventh(String docNumber) {
+        return docNumber.endsWith("1a2b");
+    }
 }
